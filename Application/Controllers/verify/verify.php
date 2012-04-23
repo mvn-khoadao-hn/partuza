7615b27f1333af2b4bfc52847d66984217d060ea
Dao Anh Khoa <khoada@lifetimetech.vn>
2012-04-23 19:20:48 +0900

    verify purchase

---
+++ Application/Controllers/verify/verify.php
@@ -0,0 +1,30 @@
+<?php
+
+class verifyController extends baseController {
+
+  public function verifyReceipt($params) {
+  
+	$myreceipt = json_encode(array('receipt-data' => $_GET['receipt']));
+	$url = "https://sandbox.itunes.apple.com/verifyReceipt";
+
+	//create cURL Request
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $myreceipt);
+
+	//execute cURL Request
+	$response = curl_exec($ch);
+	$errno = curl_errno($ch);
+	$errmsg = curl_error($ch);
+	curl_close($ch);
+
+	$response_json = json_decode($response);
+
+	//var_dump($response_json);
+	//print $response_json->{'status'};
+	print $response;
+   
+  }
+}
